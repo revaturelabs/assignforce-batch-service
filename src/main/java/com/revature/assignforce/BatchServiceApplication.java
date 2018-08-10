9bd019dc7f07c16413662218f45f0a88235a1a4a
August Duet <august.duet@revature.com>
2018-08-10 02:15:13 -0400

    profiles test

--- src/main/java/com/revature/assignforce/BatchServiceApplication.java
+++ src/main/java/com/revature/assignforce/BatchServiceApplication.java
@@ -3,10 +3,12 @@ package com.revature.assignforce;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Profile;
 
 
 @EnableDiscoveryClient
 @SpringBootApplication
+@Profile("default")
 public class BatchServiceApplication {
 
 	public static void main(String[] args) {
