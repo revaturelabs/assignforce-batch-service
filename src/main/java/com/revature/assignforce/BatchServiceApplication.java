f4b3ca20682758b9e3eb8e443f35c2fb0307a08c
August Duet <august.duet@revature.com>
2018-08-13 22:57:52 -0400

    [debug]

--- src/main/java/com/revature/assignforce/BatchServiceApplication.java
+++ src/main/java/com/revature/assignforce/BatchServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
 @EnableDiscoveryClient
