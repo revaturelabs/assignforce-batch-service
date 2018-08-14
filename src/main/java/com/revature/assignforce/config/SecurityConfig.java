89095aa92b994dbc25b0157032b7e022c44a3535
August Duet <august.duet@revature.com>
2018-08-13 23:01:33 -0400

    [debug]

--- src/main/java/com/revature/assignforce/config/SecurityConfig.java
+++ src/main/java/com/revature/assignforce/config/SecurityConfig.java
@@ -9,6 +9,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        System.out.println("USING SECURITY CONFIG");
         http
             .authorizeRequests()
             .anyRequest().permitAll();
