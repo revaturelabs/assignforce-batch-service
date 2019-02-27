7d863c1ed63a8b6d716d8aa6711ecf600e615f85
August Duet <august.duet@revature.com>
2019-02-27 16:08:57 -0500

    configuration

--- src/main/java/com/revature/assignforce/BatchServiceApplication.java
+++ src/main/java/com/revature/assignforce/BatchServiceApplication.java
@@ -10,7 +10,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableFeignClients
 @EnableCircuitBreaker
