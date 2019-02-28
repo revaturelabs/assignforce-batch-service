cc963f2e4fe5b98339ef7d755d84febc6fc3b84f
August Duet <august.duet@revature.com>
2019-02-27 22:12:00 -0500

    working on ecs deployment

--- src/main/java/com/revature/assignforce/BatchServiceApplication.java
+++ src/main/java/com/revature/assignforce/BatchServiceApplication.java
@@ -1,10 +1,8 @@
 package com.revature.assignforce;
 
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
