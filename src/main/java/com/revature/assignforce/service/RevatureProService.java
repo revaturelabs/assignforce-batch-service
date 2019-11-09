1bf4a7990d408f43b3cc1009fc3d6432f553bbe5
Thomas Anderson <thomas.anderson@ucdconnect.ie>
2019-11-09 10:13:12 -0500

    added RevatureProRestClient and RevatureProService

--- src/main/java/com/revature/assignforce/service/RevatureProService.java
+++ src/main/java/com/revature/assignforce/service/RevatureProService.java
@@ -11,7 +11,6 @@ public class RevatureProService {
 
     ArrayList<Batch> allBatches;
 
-
     RevatureProUserDTO authenticate(){
 
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
